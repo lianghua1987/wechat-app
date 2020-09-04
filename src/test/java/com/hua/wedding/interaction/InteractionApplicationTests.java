9be71d85b58e30e632ce424d78df8d265df4a38c
Hua Liang <hualiang@mercuria.com>
2020-09-04 12:01:12 +0800

    init

---
+++ src/test/java/com/hua/wedding/interaction/InteractionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hua.wedding.interaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteractionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
